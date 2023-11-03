143a484a5a3c818daedc12d155bce94d01d15c89
Erik Junsved <junsvede@gmail.com>
2023-11-03 16:58:58 +0100

    win install script test1

--- install.bat
+++ install.bat
@@ -4,3 +4,6 @@ cls
 
 REM TODO: Write Windows installation script.
 echo TODO: Write Windows installation script
+
+REM TODO: Write Windows installation script.
+echo TODO: Write Windows installation script2
